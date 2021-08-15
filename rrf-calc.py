b2b62762396282a66f8399db46f66bbd44ccfc69
Tales Carvalho <tales-h-carvalho@hotmail.com>
2021-08-15 14:54:07 -0300

    Files processing with pandas

--- rrf-calc.py
+++ rrf-calc.py
@@ -1,1 +1,16 @@
-# TO-DO
+import glob
+import os
+import pandas as pd
+
+if __name__ == '__main__':
+  inputFiles = glob.glob('input/*.csv')
+  print(f'Found {len(inputFiles)} input files:')
+  [print(f'\t{os.path.basename(f)}') for f in inputFiles]
+
+  for f in inputFiles:
+    df = pd.read_csv(f)
+
+    df['Classification'] = '---'
+    df['RRF'] = 1
+
+    df.to_csv(f'output/{os.path.basename(f)}', index=False)
